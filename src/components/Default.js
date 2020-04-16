f22c8b68498cfa4f246bc9b80bace733019f4ac4
eersoy84 <eersoy84@gmail.com>
2020-04-16 23:46:42 +0300

    default sayfası yapıldı

--- src/components/Default.js
+++ src/components/Default.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react'
 
 export default class Default extends Component {
     render() {
+        console.log(this.props)
         return (
-            <div>
-                <h3>Page Not Found</h3>
-            </div>
-        )
+                 <div className="container">
+                     <div className="row">
+                         <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+                             <h1 className="display-3">404</h1>
+                             <h1>error</h1>
+                             <h1>page not found</h1>
+                             <h3>The requested URL was not found<span className="text-danger">
+                                 {this.props.location.pathname}
+                             </span>{""} was not found</h3>
+                         </div>
+                     </div>
+                 </div>
+            )
     }
 }
